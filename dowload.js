eef63f06130c1886147c369efeaf288386fed234
Allan <92821387+TREPALAN@users.noreply.github.com>
2024-05-15 18:11:04 -0300

    Update dowload.js

--- dowload.js
+++ dowload.js
@@ -2,9 +2,7 @@
  * Reencode audio & video without creating files first
  *
  * Requirements: ffmpeg, ether via a manual installation or via ffmpeg-static
- *
- * If you need more complex features like an output-stream you can check the older, more complex example:
- * https://github.com/fent/node-ytdl-core/blob/cc6720f9387088d6253acc71c8a49000544d4d2a/example/ffmpeg.js
+ 
  */
 
 // Built-in with nodejs
